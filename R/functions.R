6f5ec564caf09a523cc4c56635fb55f206281c27
Alexander Woods <alexhwoods@gatech.edu>
2016-11-21 21:53:36 -0500

    Commented clean_cats really thoroughly

--- R/functions.R
+++ R/functions.R
@@ -14,13 +14,25 @@
 ##################
 ## clean_cats() ##
 ##################
+# This function returns a vector that is the the categories (presumably)
+# that exist in the input vector 'input_nm', but only if they're above a 
+# certain threshold. If not, they're thrown into the "Other" category.
 clean_cats = function(df, input_nm,threshold){
+  # creates a new column in character form of input_nm
   df$cat = as.character(df[,input_nm])
+  
+  # replaces NA with "Unkown for all rows in df
   df$cat[is.na(df$cat)] = "Unknown"
+  
+  # df = group_by(df, cat)
+  # Then create a new column in df that is percent_of_n / total rows
   df <- df %>% 
     group_by(cat) %>% 
     mutate(pct_n = n()/NROW(df))
   
+  # creates new column called 'cat2' that is the same as 
+  # df$cat, UNLESS df$pct_n is above threshold, then df$cat2
+  # for that row is "Other"
   df$cat2 = ifelse(df$pct_n>threshold, df$cat, "Other")
   
   return(df$cat2)
